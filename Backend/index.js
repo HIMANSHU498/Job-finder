f334cc60fa5a25702c9e1ef78e9649a043f40eb5
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-06-14 14:30:07 +0530

    “added-login/register-routes-and-jwt-tokenization”

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,56 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const User = require("./models/Users");
 const jobDetails = require("./models/JobDetails");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 dotenv.config();
-app.get("", (req, resp) => {
-  resp.send("all good");
+app.use(express.json());
+app.get("/healthstatus", (req, resp) => {
+  resp.send("Website working fine");
+});
+app.post("/register", async (req, resp) => {
+  try {
+    const { name, email, mobile, password } = req.body;
+    if (!name || !email || !password || !mobile) {
+      return resp.json("All fields are required");
+    }
+    const alreadyUser = await User.findOne({ email });
+    if (alreadyUser) {
+      return resp.json({ error: "User already exists" });
+    }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const userData = new User({
+      name,
+      email,
+      password: encryptedPassword,
+      mobile,
+    });
+    const savedUser = await userData.save();
+    const jwtToken = jwt.sign({ email }, process.env.SECRET_KEY);
+    resp.json({ savedUser, jwtToken });
+  } catch (error) {
+    resp.send({ error: "Something went wrong" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json("All fields are required");
+    }
+    const checkEmail = await User.findOne({ email });
+    if (checkEmail) {
+      const checkPassword = await bcrypt.compare(password, checkEmail.password);
+      if (checkPassword) {
+        const jwtToken = jwt.sign({ email }, process.env.SECRET_KEY);
+        return res.json({ message: "User logged in successfully", jwtToken });
+      }
+    }
+    return res.json({ message: "Invalid credentials" });
+  } catch (error) {
+    return res.json({ error: "" });
+  }
 });
 app.listen(process.env.PORT, () => {
   mongoose
