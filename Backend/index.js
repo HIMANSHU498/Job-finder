c5a8e4124ff12333396d887f462b77c2c997c58a
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-06-15 13:49:38 +0530

    added error handler middleware

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,15 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 app.use(express.json());
+app.use((err, req, res, next) => {
+  res.status(err.satus || 500);
+  res.send({
+    error: {
+      status: err.satus || 500,
+      message: err.message,
+    },
+  });
+});
 app.get("/healthstatus", (req, resp) => {
   resp.send("Website working fine");
 });
@@ -15,11 +24,11 @@ app.post("/register", async (req, resp) => {
   try {
     const { name, email, mobile, password } = req.body;
     if (!name || !email || !password || !mobile) {
-      return resp.json("All fields are required");
+      return resp.status(401).json("All fields are required");
     }
     const alreadyUser = await User.findOne({ email });
     if (alreadyUser) {
-      return resp.json({ error: "User already exists" });
+      return resp.status(400).json({ error: "User already exists" });
     }
     const encryptedPassword = await bcrypt.hash(password, 10);
     const userData = new User({
@@ -40,17 +49,19 @@ app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.json("All fields are required");
+      return res.status(400).json("All fields are required");
     }
     const checkEmail = await User.findOne({ email });
     if (checkEmail) {
       const checkPassword = await bcrypt.compare(password, checkEmail.password);
       if (checkPassword) {
         const jwtToken = jwt.sign({ email }, process.env.SECRET_KEY);
-        return res.json({ message: "User logged in successfully", jwtToken });
+        return res
+          .status(200)
+          .json({ message: "User logged in successfully", jwtToken });
       }
     }
-    return res.json({ message: "Invalid credentials" });
+    return res.status(400).json({ message: "Invalid credentials" });
   } catch (error) {
     return res.json({ error: "" });
   }
