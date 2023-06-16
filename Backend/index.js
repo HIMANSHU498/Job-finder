124b424b799509f7a2fd7ada8dccc9d03d01ccf1
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-06-16 13:48:28 +0530

    createjob post api done

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ const User = require("./models/Users");
 const jobDetails = require("./models/JobDetails");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const IsAuthenticated = require("./middlewares/IsAuthenticated");
 dotenv.config();
 app.use(express.json());
 app.use((err, req, res, next) => {
@@ -24,7 +25,7 @@ app.post("/register", async (req, resp) => {
   try {
     const { name, email, mobile, password } = req.body;
     if (!name || !email || !password || !mobile) {
-      return resp.status(401).json("All fields are required");
+      return resp.status(401).json("Some fields are required");
     }
     const alreadyUser = await User.findOne({ email });
     if (alreadyUser) {
@@ -49,7 +50,7 @@ app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json("All fields are required");
+      return res.status(400).json("Some fields are required");
     }
     const checkEmail = await User.findOne({ email });
     if (checkEmail) {
@@ -64,6 +65,53 @@ app.post("/login", async (req, res) => {
     return res.status(400).json({ message: "Invalid credentials" });
   } catch (error) {
     return res.json({ error: "" });
+  }
+});
+
+app.post("/createjobpost", IsAuthenticated, async (req, res) => {
+  try {
+    const {
+      companyName,
+      logoURL,
+      jobPosition,
+      salary,
+      jobType,
+      jobMode,
+      location,
+      jobDescription,
+      aboutCompany,
+      skillsRequired,
+    } = req.body;
+    if (
+      !companyName ||
+      !logoURL ||
+      !jobPosition ||
+      !salary ||
+      !jobType ||
+      !jobMode ||
+      !location ||
+      !jobDescription ||
+      !aboutCompany ||
+      !skillsRequired
+    ) {
+      return res.status(401).json({ error: "All fields are required" });
+    }
+    const newJobPost = new jobDetails({
+      companyName,
+      logoURL,
+      jobPosition,
+      salary,
+      jobType,
+      jobMode,
+      location,
+      jobDescription,
+      aboutCompany,
+      skillsRequired,
+    });
+    const result = await newJobPost.save();
+    return res.json({ message: "Job-post created successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
   }
 });
 app.listen(process.env.PORT, () => {
